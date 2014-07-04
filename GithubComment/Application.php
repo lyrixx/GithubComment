986a79a7705de61b7de78bd8a77f4673d8a90934
Grégoire Pineau <lyrixx@lyrixx.info>
2014-07-04 12:34:45 +0200

    Prepare release v0.0.3

--- GithubComment/Application.php
+++ GithubComment/Application.php
@@ -11,7 +11,7 @@ class Application extends BaseApplication
 {
     public function __construct()
     {
-        parent::__construct('GithubComment', '0.0.2');
+        parent::__construct('GithubComment', '0.0.3');
 
         $this->add(new Commands\CommentCommand());
     }
